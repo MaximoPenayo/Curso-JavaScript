e2f5dbe24e63f17b32080fb446de12325f4eaf88
Maximo_Penayo <maximojpenayo@gmail.com>
2025-05-15 02:33:48 -0300

    Hola

---
+++ Operadores en JavaScript/incrementoDecremento.js
@@ -0,0 +1,16 @@
+// Operadores Incremento / Decremento
+let a,b,console
+a = 0;
+// Pre-Incremento
+++a;
+console.log(a)
+// Post-Incremento
+a++
+console.log(a)
+
+// Pre-Decremento
+--a;
+console.log(a)
+// Post-Decremento
+a--
+console.log(a)
---
+++ Operadores en JavaScript/operadoresAritmeticos.js
@@ -0,0 +1,29 @@
+let a,b,c,d,e,f;
+
+// Suma
+a = 3 + 4;
+console.log(a);
+
+// Resta
+b = 6 - 2;
+console.log(b);
+
+// Multiplicacion
+c = a * 2;
+console.log(c);
+
+// Division
+d = b / 2.5;
+console.log(d);
+
+// Modulo
+f = 9 % 2;
+console.log(f);
+
+// Potencia
+e = 2 ** 3;
+console.log(e);
+
+// Potencia
+e = Math.pow(2,3);
+console.log(e);
